376a12cb36de50c9d405ef6de01065836e7c9909
Thor Christiansen <Thor@fipros.dk>
2024-08-08 12:28:20 +0200

    Added a simplified way of adding project to service collection.

---
+++ EasySoapClient/EasySoapClient/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using EasySoapClient.Interfaces;
+using EasySoapClient.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EasySoapClient.Extensions;
+
+public static class IServiceCollectionExtensions
+{
+    public static void AddEasySoapClient(this IServiceCollection services)
+    {
+        services.AddTransient<IRepositoryFactory, RepositoryFactory>();
+    };
+}
